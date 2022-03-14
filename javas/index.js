84d5834a90600c6b9fe87220a43ed9617059c2d9
tanvi008 <tanvirupareliya84@gmail.com>
2022-03-14 18:30:42 +0530

    django_files

--- javas/index.js
+++ javas/index.js
@@ -18,9 +18,41 @@ for (x of buttons){
         else if (buttonText == "="){
             display.value = eval(displayValue);
         }
+        
+        else if (buttonText == "MC"){
+            display.value = "0";
+            display.value = displayValue;
+
+        }
+        else if(buttonText == 'sin'){
+            display.value = Math.sin(display.value );
+        }
+        else if(buttonText == 'cos'){
+            display.value  = Math.cos(display.value );
+        }
+        else if(buttonText == 'tan'){
+            display.value = Math.tan(display.value );
+        }
+        else if(buttonText == 'log'){
+            display.value  = Math.log(display.value );
+        }
+        else if(buttonText == '1/X'){
+            display.value  = 1 / eval(display.value );
+        }
+        else if(buttonText == 'MOD'){
+             
+            displayValue += '%';
+            display.value  = displayValue ;
+
+        }
+        
+        else if(buttonText =='X²' ){
+            display.value = eval(display.value) * eval(display.value);
+        }
         else{
             displayValue += buttonText;
             display.value = displayValue;
+
         }
     })
 
