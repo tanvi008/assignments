1c69ed452e53b8ea5fefb62e73e58f92f0315af8
tanvi008 <tanvirupareliya84@gmail.com>
2022-03-22 18:50:21 +0530

    final django

---
+++ devsearch3/devsearch1/users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Profile
+# Create your views here.
+
+
+def profiles(request):
+    profiles = Profile.objects.all()
+    context = {'profiles':profiles}
+    return render(request, 'users/profiles.html', context)
+     
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id =pk)
+    context ={'profile':profile}
+    return render(request, 'users/user-profile.html', context)
+
+    
